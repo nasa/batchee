ce5531b0e9db4c17034883a5fcdb61705afa3d15
danielfromearth <daniel.kaufman@nasa.gov>
2024-10-21 10:35:39 -0400

    use newer harmony_service_lib, with updated package name, and remove upper pins

--- batcher/harmony/cli.py
+++ batcher/harmony/cli.py
@@ -27,12 +27,12 @@
 """A Harmony CLI wrapper around the concatenate-batcher"""
 from argparse import ArgumentParser
 
-import harmony
+import harmony_service_lib
 
 from batcher.harmony.service_adapter import ConcatBatching as HarmonyAdapter
 
 
-def main(config: harmony.util.Config = None) -> None:
+def main(config: harmony_service_lib.util.Config = None) -> None:
     """Parse command line arguments and invoke the service to respond to them.
 
     Parameters
@@ -47,10 +47,10 @@ def main(config: harmony.util.Config = None) -> None:
     parser = ArgumentParser(
         prog="Pre-concatenate-batching", description="Run the pre-concatenate-batching service"
     )
-    harmony.setup_cli(parser)
+    harmony_service_lib.setup_cli(parser)
     args = parser.parse_args()
-    if harmony.is_harmony_cli(args):
-        harmony.run_cli(parser, args, HarmonyAdapter, cfg=config)
+    if harmony_service_lib.is_harmony_cli(args):
+        harmony_service_lib.run_cli(parser, args, HarmonyAdapter, cfg=config)
     else:
         parser.error("Only --harmony CLIs are supported")
 
