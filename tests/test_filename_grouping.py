65186a1ff90d34de0c5efb243fe05872447d157d
Andrey <andrey.kim@nasa.gov>
2025-08-19 23:08:31 -0400

    update tests with the case when granule names cross midnight in UTC timezone

--- tests/test_filename_grouping.py
+++ tests/test_filename_grouping.py
@@ -2,22 +2,29 @@ import sys
 from unittest.mock import patch
 
 import batcher.tempo_filename_parser
-from batcher.tempo_filename_parser import get_batch_indices
+from batcher.tempo_filename_parser import get_batch_indices, get_day_in_us_central
 
 example_filenames = [
-    "TEMPO_HCHO_L2_V01_20130701T212354Z_S009G05.nc",
-    "TEMPO_HCHO_L2_V01_20130701T212953Z_S009G06.nc",
-    "TEMPO_HCHO_L2_V01_20130701T213553Z_S009G07.nc",
-    "TEMPO_HCHO_L2_V01_20130701T215955Z_S010G01.nc",
-    "TEMPO_HCHO_L2_V01_20130701T220554Z_S010G02.nc",
-    "TEMPO_HCHO_L2_V01_20130701T221154Z_S010G03.nc",
+    "TEMPO_NO2_L2_V03_20240731T235252Z_S016G04.nc",
+    "TEMPO_NO2_L2_V03_20240731T235929Z_S016G05.nc",
+    "TEMPO_NO2_L2_V03_20240801T000606Z_S016G06.nc",
+    "TEMPO_NO2_L2_V03_20240801T001302Z_S017G01.nc",
+    "TEMPO_NO2_L2_V03_20240801T001942Z_S017G02.nc",
+    "TEMPO_NO2_L2_V03_20240801T002619Z_S017G03.nc",
+    "TEMPO_NO2_L2_V03_20240801T233313Z_S016G01.nc",
+    "TEMPO_NO2_L2_V03_20240801T233953Z_S016G02.nc",
+    "TEMPO_NO2_L2_V03_20240801T234630Z_S016G03.nc",
 ]
 
+def test_timezone_conversion():
+    utcdates = [filename.split('_')[4] for filename in example_filenames]
+    days_in_central = [get_day_in_us_central(utcdt[0:8], utcdt[9:15]) for utcdt in utcdates]
+    assert days_in_central == ["20240731", "20240731", "20240731", "20240731", "20240731", "20240731", "20240801", "20240801", "20240801"]
 
 def test_grouping():
     results = get_batch_indices(example_filenames)
 
-    assert results == [0, 0, 0, 1, 1, 1]
+    assert results == [0, 0, 0, 1, 1, 1, 2, 2, 2]
 
 
 def test_main_cli():
@@ -27,4 +34,4 @@ def test_main_cli():
     with patch.object(sys, "argv", test_args):
         grouped_names = batcher.tempo_filename_parser.main()
 
-    assert grouped_names == [example_filenames[0:3], example_filenames[3:6]]
+    assert grouped_names == [example_filenames[0:3], example_filenames[3:6], example_filenames[6:9]]
